a989e13e1a1b30ba3eb57e210042df59bfbd9fbb
ValeOlivares <valentinaolivares85@gmail.com>
2018-02-21 23:15:59 -0300

    botoncito (falta arreglar posicion)

--- assets/js/app.js
+++ assets/js/app.js
@@ -172,20 +172,20 @@ $('#searchBar').submit(function (e) {
 				console.log(data[i].imgGold);
 
 				$('#recomendation').append(
-				`
-				<div class="cardContainer col m3 s2">
-				<img class="responsive-img" src="${data[i].imgGold}">
-				<div class="fixed-action-btn horizontal click-to-toggle"><a>
-    	<img src="assets/images/icon.png" alt="" class="material-icons icon"  >
-    </a>
-    <ul>
-      <li><a class="btn-floating red"><i class="material-icons">insert_chart</i></a></li>
-      <li><a class="btn-floating yellow darken-1"><i class="material-icons">format_quote</i></a></li>
-      <li><a class="btn-floating green"><i class="material-icons">publish</i></a></li>
-      <li><a class="btn-floating blue"><i class="material-icons">attach_file</i></a></li>
-    </ul>
-  </div>
- 				</div>`
+				`<div class=" col m3 s2">` +
+					`<img class="responsive-img" src="${data[i].imgGold}">` +
+					`<div class="fixed-action-btn horizontal click-to-toggle buttons">` +
+					`<a>` +
+    					`<img src="assets/images/icon.png" alt="" class="material-icons icon"> ` +
+   					 `</a>` +
+   						 `<ul>` +
+     						 `<li><a class="btn-floating yellow darken-1"><i class="material-icons">favorite_border</i></a></li>` +
+     					 	`<li><a class="btn-floating yellow darken-1"><i class="material-icons">info_outline</i></a></li>` +
+     					 	`<li><a class="btn-floating yellow darken-1"><i class="material-icons">publish</i></a></li>`+
+      						`<li><a class="btn-floating yellow darken-1"><i class="material-icons">attach_file</i></a></li>` +
+    					`</ul>` +
+  					`</div>` +
+ 				`</div>`
 				);
 			}
 		}
